616c470624125582d06c73161ece5131792bdb7d
Quang Mai <maithanhnhatquang1@gmail.com>
2024-12-04 20:35:28 +0700

    Finish header navbar

--- project/retail/views.py
+++ project/retail/views.py
@@ -5,3 +5,11 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, "retail/index.html")
+
+
+def contact(request):
+    return render(request, "retail/contact.html")
+
+
+def about(request):
+    return render(request, "retail/about.html")
